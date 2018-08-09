c5eaae756107720aaeec15c204587c582861fdcf
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-08-09 18:51:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+ let rankedList = document.querySelectorAll('.ranked-list li');
+ for(let i = 0; i < rankedList.length; i++) {
+   rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
+ }
+return rankedList;
+  
+}
+
+function deepestChild() {
+  const grand_node = document.getElementById('grand-node');
+  return grand_node.querySelector('div').querySelector('div').querySelector('div').querySelector('div');
+}
