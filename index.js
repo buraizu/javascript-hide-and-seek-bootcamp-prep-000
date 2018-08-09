5e19a4cc7742b6b54967e61f19fca0dd1a8b2e21
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-08-09 18:11:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+ let rankedList = document.querySelectorAll('.ranked-list li');
+ for(let i = 0; i < rankedList.length; i++) {
+   rankedList[i] = parseInt(rankedList[i]) + n;
+ }
+
+ 
+  
+}
+
+function deepestChild() {
+  const grand_node = document.getElementById('grand-node');
+  return grand_node.querySelector('div').querySelector('div').querySelector('div').querySelector('div');
+}
